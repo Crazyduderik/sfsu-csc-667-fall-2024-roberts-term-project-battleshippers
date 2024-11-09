93f8110473a5e5e58af75cf4bbad5cb6ec511fd7
Angelo Arriaga <martyr@Angelos-Air.hsd1.ca.comcast.net>
2024-11-08 20:25:47 -0800

    editted auth route

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -2,7 +2,11 @@ import express from "express";
 const authRoutes = express.Router();
 
 authRoutes.get("/", (req, res) => {
-  res.send("Auth route hello");
+  res.render("login", { title: "battleshippers's site" });
+});
+
+authRoutes.get("/register", (req, res) => {
+  res.render("register", { title: "battleshippers's site" });
 });
 
 export default authRoutes;
