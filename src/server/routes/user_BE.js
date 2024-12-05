a0fd42a4abf20824ccbf422b3065ce744a2ab246
Martyr <Angelo.S.Arriaga@outlook.com>
2024-12-04 20:58:01 -0800

    Formatting Tabs

--- src/server/routes/user_BE.js
+++ src/server/routes/user_BE.js
@@ -20,15 +20,14 @@ router.post('/login', async (req, res) => {//TODO: API VERIFY LOGGED OUT (can't 
 	try {
 		const [results] = await connection.execute("SELECT * FROM users WHERE username = ?", [username]);
 		// If user does not exist,
-    // Return Error
+		// Return Error
 		if (results.length === 0)
-      {return res.status(400).json({error: "Invalid Username or Password" });}
+			{return res.status(400).json({error: "Invalid Username or Password" });}
 		const user = results[0];
 
 		const isMatch = await bcrypt.compare(password, user.password_hash);
-		if (!isMatch) {
-			return res.status(400).json({ error: 'Invalid Username or Password' });
-		}
+		if (!isMatch)
+			{return res.status(400).json({ error: 'Invalid Username or Password' });}
 
 		const [sessionResults] = await connection.execute('SELECT * FROM sessions WHERE user_id = ? AND session_end IS NULL', [user.uuid]);
 
@@ -48,7 +47,7 @@ router.post('/login', async (req, res) => {//TODO: API VERIFY LOGGED OUT (can't 
                             );
 		return res.json({ message: "Login Successful!" });
 	} catch (err)
-    {return res.json({ error: err });}
+    		{return res.json({ error: err });}
 });
 
 // Registration 
@@ -58,39 +57,39 @@ router.post('/register', async (req, res) => {//TODO: HAVE API VERIFICATION THAT
 
 	// Passwords must match
 	if (password !== verify_password)
-    {return res.status(400).json({ error: "Passwords do not match!" });}
+		{return res.status(400).json({ error: "Passwords do not match!" });}
 
 	// Usernames and Passwords must meet criteria:
-  // >5 characters for username
-  // >8 characters for password
+  	// >5 characters for username
+  	// >8 characters for password
 	if (username.length < 5)
-    {return res.status(400).json({ error: "Username must be at least 5 characters" });}
+    		{return res.status(400).json({ error: "Username must be at least 5 characters" });}
 
 	if (password.length < 8)
-    {return res.status(400).json({ error: "Password must be at least 8 characters" });}
+		{return res.status(400).json({ error: "Password must be at least 8 characters" });}
 
 	try {
 		if (!(await universityEmailAuthentication(email)))
-      {return res.status(400).json({ error: "Email must be a valid university email" });}
+			{return res.status(400).json({ error: "Email must be a valid university email" });}
 
 		const [emailResults] = await connection.execute("SELECT * FROM users WHERE email = ?", [email]);
 		// If email exists,
-    // return error
+		// return error
 		if (emailResults.length > 0)
-      {return res.status(400).json({ error: "Email already exists" });}
+			{return res.status(400).json({ error: "Email already exists" });}
 
 		const [userResults] = await connection.execute("SELECT * FROM users WHERE username = ?", [username]);
 		// If username exists,
-    // return error
+    		// return error
 		if (userResults.length > 0)
-      {return res.status(400).json({ error: "Username already exists" });}
+			{return res.status(400).json({ error: "Username already exists" });}
 
 		// Hash the password with bcrypt
-    // Insert into database
+		// Insert into database
 		const hash = await bcrypt.hash(password, 10);
 		await connection.execute("INSERT INTO users (uuid, email, username, password_hash) VALUES (UUID(), ?, ?, ?)", [email, username, hash]);
 		
-    return res.json({ message: "User registered successfully" });//TODO: Status Code?
+		return res.json({ message: "User registered successfully" });//TODO: Status Code?
     
 	} catch (err) {
 		console.error("Error during registration:", err);
